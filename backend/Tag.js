a3abd4922c2e56b549cacb1944ff8aa0c06e6097
martin-rod06 <mrodr313@calpoly.edu>
2024-11-06 18:58:20 -0800

    1st implementation of Tag

---
+++ backend/Tag.js
@@ -0,0 +1,16 @@
+class Tag{
+    constructor(tag){
+        this.tag = tag;
+        this.tagID;
+    }
+    getTag(){
+        return this.tag;
+    }
+    getTagID(){
+        return this.tagID;
+    }
+    setTag(newtag){
+        this.tag = newtag;
+    }
+}
+
