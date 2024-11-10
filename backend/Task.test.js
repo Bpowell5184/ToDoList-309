d8d047f32f8d8cdac0245c841c06c9a96e107a69
watanabed0706 <105673471+watanabed0706@users.noreply.github.com>
2024-11-10 15:39:22 -0800

    Additional Task Tests

--- backend/Task.test.js
+++ backend/Task.test.js
@@ -1,18 +1,75 @@
 const Task = require("./Task");
+const Tag = require("./Tag");
 
-test('Does The Task Constructor Work?', () => {
+test('Does The Task Constructor & Getters Work?', () => {
     const result = new Task();
 
-    expect(result.title).toMatch("untitled");
-    expect(result.title).not.toMatch("THIS IS THE WRONG TITLE");
-    expect(result.description).toMatch("description");
-    expect(result.description).not.toMatch("THIS IS THE WRONG DESCRIPTION");
-    expect(result.points).toBe(5);
-    expect(result.points).not.toBe(100);
-    expect(result.priority).toBe(5);
-    expect(result.priority).not.toBe(33);
-    expect(result.tagsList).toEqual([]);
-    expect(result.tagsList).not.toEqual(["Some Object"]);
-    expect(result.completionSatus).toBeFalsy();
-    expect(result.completionSatus).not.toBeTruthy();
+    expect(result.getTitle()).toMatch("untitled");
+    expect(result.getTitle()).not.toMatch("THIS IS THE WRONG TITLE");
+    expect(result.getDescription()).toMatch("description");
+    expect(result.getDescription()).not.toMatch("THIS IS THE WRONG DESCRIPTION");
+    expect(result.getPoints()).toBe(5);
+    expect(result.getPoints()).not.toBe(100);
+    expect(result.getPriority()).toBe(5);
+    expect(result.getPriority()).not.toBe(33);
+    expect(result.getTagsList()).toEqual([]);
+    expect(result.getTagsList()).not.toEqual(["Some Object"]);
+    expect(result.getCompletionStatus()).toBeFalsy();
+    expect(result.getCompletionStatus()).not.toBeTruthy();
+})
+
+test('Does setTitle Work?', () => {
+    const result = new Task();
+    
+    result.setTitle("Eat a Large Mango.");
+    expect(result.getTitle()).toMatch("Eat a Large Mango.");
+
+    result.setTitle("Eat a Small Pineapple.");
+    expect(result.getTitle()).toMatch("Eat a Small Pineapple.");
+
+    result.setTitle("");
+    expect(result.getTitle()).toMatch("");
+    expect(result.getTitle()).not.toMatch("Eat a Small Pineapple.");
+})
+
+test('Does setDescription Work?', () => {
+    const result = new Task();
+    
+    result.setDescription("Eat a Large Mango.");
+    expect(result.getDescription()).toMatch("Eat a Large Mango.");
+
+    result.setDescription("Eat a Small Pineapple.");
+    expect(result.getDescription()).toMatch("Eat a Small Pineapple.");
+
+    result.setDescription("");
+    expect(result.getDescription()).toMatch("");
+    expect(result.getDescription()).not.toMatch("Eat a Small Pineapple.");
+})
+
+test('Does setPoints Work?', () => {
+    const result = new Task();
+    
+    result.setPoints(0);
+    expect(result.getPoints()).toBe(0);
+
+    result.setPoints(10);
+    expect(result.getPoints()).toBe(10);
+
+    result.setPoints(5);
+    expect(result.getPoints()).toBe(5);
+    expect(result.getPoints()).not.toBe(3);
+})
+
+test('Does setPriority Work?', () => {
+    const result = new Task();
+    
+    result.setPriority(0);
+    expect(result.getPriority()).toBe(0);
+
+    result.setPriority(10);
+    expect(result.getPriority()).toBe(10);
+
+    result.setPriority(5);
+    expect(result.getPriority()).toBe(5);
+    expect(result.getPriority()).not.toBe(3);
 })
