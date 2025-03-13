4d582827157d6730c8e2ef1875765a6eb88d796d
legion5184 <96603312+legion5184@users.noreply.github.com>
2025-03-13 10:26:27 -0700

    messing with cors to fix backend

--- backend/backend.js
+++ backend/backend.js
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 8700;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Not sure any of this works as it was from before updates to userservices and taskservices:
