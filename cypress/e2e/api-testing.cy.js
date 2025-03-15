42bc7d5043439b8fe430c8203a9a2aadde0f1f1c
watanabed0706 <105673471+watanabed0706@users.noreply.github.com>
2025-03-14 21:27:12 -0700

    Automated Acceptance Testing with Cypress

---
+++ cypress/e2e/api-testing.cy.js
@@ -0,0 +1,108 @@
+const BACKEND_URL = 'http://localhost:8700';
+
+describe('Backend (REST API) is listening', () => {
+  context('Backend loads and runs successfully', () => {
+    before(() => {});
+  });
+  it('GIVEN I run backend', () => {});
+
+  it('WHEN I visit root (/) end point', () => {
+    cy.request(BACKEND_URL + '/').then((response) => {
+      assert.isNotNull(response.body, 'THEN ');
+      assert.equal(
+        response.body,
+        'Welcome to the Backend',
+        'AND returns the correct body text',
+      );
+      assert.equal(
+        response.status,
+        200,
+        'AND the response code is successful (200)!',
+      );
+    });
+  });
+});
+
+describe('API POSTs, GETs, DELETEs a User from the DB', () => {
+  context('Successful Post', () => {
+    before(() => {});
+
+    let user = {};
+    let userId;
+
+    it('GIVEN User obj has valid attributes', () => {
+      user = {
+        username: 'cypressAPItestuser',
+        name: 'Cypress',
+        password: 'iLikeToMoveItMoveIt!3',
+      };
+    });
+
+    it('WHEN I attempt to POST a new user', () => {
+      cy.request('POST', BACKEND_URL + '/adduser', user).then((response) => {
+        assert.equal(
+          response.status,
+          201,
+          'THEN I recieve a successful response (201)',
+        );
+        assert.equal(
+          response.body.user.username,
+          user.username,
+          'AND response contains user info',
+        );
+        assert.equal(
+          response.body.user.name,
+          user.name,
+          'AND response contains user info',
+        );
+        assert.exists(
+          response.body.user._id,
+          'AND response contains user info',
+        );
+        userId = response.body.user._id;
+      });
+    });
+
+    it('WHEN I attempt to GET an existing User', () => {
+      cy.request('POST', BACKEND_URL + '/getuser', {
+        username: user.username,
+        password: user.password,
+      }).then((response) => {
+        assert.equal(response.status, 200, 'THEN the get call was successful');
+        assert.equal(
+          response.body.user.username,
+          user.username,
+          'AND the username is correct',
+        );
+        assert.equal(
+          response.body.user.name,
+          user.name,
+          'AND the name is correct',
+        );
+        assert.exists(response.body.user._id, 'AND ID exists');
+      });
+    });
+
+    it('WHEN I attempt to DELETE an existing User', () => {
+      cy.request('DELETE', BACKEND_URL + '/deleteuser/' + userId).then(
+        (response) => {
+          assert.equal(
+            response.status,
+            200,
+            'THEN the user is deleted successfully',
+          );
+          assert.equal(
+            response.body.user.username,
+            user.username,
+            "AND I DELETE'd the correct user",
+          );
+          assert.equal(
+            response.body.user.name,
+            user.name,
+            "AND I DELETE'd the correct user",
+          );
+        },
+      );
+    });
+  });
+});
---
+++ cypress/e2e/ui-testing.cy.js
@@ -0,0 +1,132 @@
+const FRONTEND_URL = 'http://localhost:3000';
+const BACKEND_URL = 'http://localhost:8700';
+
+describe('Adding a new User', () => {
+  context('GIVEN the SignUp Site is running properly', () => {
+    it('THEN I should be able to Create an Account', () => {
+      cy.visit(FRONTEND_URL + '/signup');
+      cy.intercept('POST', BACKEND_URL + '/adduser').as('adduser');
+
+      /*Enter Login Info & Create Account*/
+      cy.get('input[placeholder="Enter new username"]').type(
+        'CypressUItestuser',
+      );
+      cy.get('input[placeholder="Enter new password"]').type(
+        'iLikeToMoveItMoveIt!3',
+      );
+      cy.get('input[placeholder="Enter your name"]').type('Cypress');
+      cy.contains('Create Account').click();
+
+      /*Verify an API call was made*/
+      cy.wait('@adduser');
+    });
+
+    it("AND I shouldn't be able to Create an Account with a pre-existing username", () => {
+      cy.visit(FRONTEND_URL + '/signup');
+      cy.intercept('POST', BACKEND_URL + '/adduser', {
+        statusCode: 400,
+        body: { error: 'Error Adding User' },
+      }).as('adduser');
+
+      /*Enter Login Info & Create Account*/
+      cy.get('input[placeholder="Enter new username"]').type(
+        'CypressUItestuser',
+      );
+      cy.get('input[placeholder="Enter new password"]').type(
+        'iLikeToMoveItMoveIt!3',
+      );
+      cy.get('input[placeholder="Enter your name"]').type('Cypress');
+      cy.contains('Create Account').click();
+
+      /*Verify an API call was made*/
+      cy.wait('@adduser');
+
+      cy.get('.error-message')
+        .should('be.visible')
+        .and('contain', 'An error occurred. Please try again.');
+    });
+  });
+});
+
+describe('Logging In as an Existing User', () => {
+  context('GIVEN the Login Site is running properly', () => {
+    it('THEN I should be able to Log In', () => {
+      cy.visit(FRONTEND_URL + '/login');
+      cy.intercept(BACKEND_URL + '/getuser').as('getuser');
+
+      /*Enter Login Info & Create Account*/
+      cy.get('input[placeholder="Enter your username"]').type(
+        'CypressUItestuser',
+      );
+      cy.get('input[placeholder="Enter your password"]').type(
+        'iLikeToMoveItMoveIt!3',
+      );
+      cy.get('button').contains('Log In').click();
+
+      /*Verify an API call was made*/
+      cy.wait('@getuser');
+    });
+
+    it("THEN I shouldn't be able to Log In without typing anything", () => {
+      cy.visit(FRONTEND_URL + '/login');
+
+      /*Don't Enter Login*/
+
+      /*Click Log In*/
+      cy.get('button').contains('Log In').click();
+
+      /*Verify Error is shown*/
+      cy.get('.error-message')
+        .should('be.visible')
+        .and('contain', 'Please enter a username and password.');
+    });
+
+    it("THEN I shouldn't be able to Log In with an invalid password", () => {
+      cy.visit(FRONTEND_URL + '/login');
+      cy.intercept(BACKEND_URL + '/getuser', {
+        statusCode: 401,
+      }).as('getuser');
+
+      /*Enter Login Info & Create Account*/
+      cy.get('input[placeholder="Enter your username"]').type(
+        'CypressUItestuser',
+      );
+      cy.get('input[placeholder="Enter your password"]').type(
+        'iLikeToMoveItMoveIt!3',
+      );
+      cy.get('button').contains('Log In').click();
+
+      /*Verify an API call was made*/
+      cy.wait('@getuser');
+
+      /*Verify Error is shown*/
+      cy.get('.error-message')
+        .should('be.visible')
+        .and('contain', 'Password not valid');
+    });
+
+    it("THEN I shouldn't be able to Log In with an nonexistant username", () => {
+      cy.visit(FRONTEND_URL + '/login');
+      cy.intercept(BACKEND_URL + '/getuser', {
+        statusCode: 404,
+      }).as('getuser');
+
+      /*Enter Login Info & Create Account*/
+      cy.get('input[placeholder="Enter your username"]').type(
+        'CypressUItestuser',
+      );
+      cy.get('input[placeholder="Enter your password"]').type(
+        'iLikeToMoveItMoveIt!3',
+      );
+      cy.get('button').contains('Log In').click();
+
+      /*Verify an API call was made*/
+      cy.wait('@getuser');
+
+      /*Verify Error is shown*/
+      cy.get('.error-message')
+        .should('be.visible')
+        .and('contain', 'Username not found');
+    });
+  });
+});
